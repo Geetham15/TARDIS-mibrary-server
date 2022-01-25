09ba7a5b743d5d9e6bc9b3281389019916c6651f
Koltan Huget <koltanhuget@hotmail.com>
2022-01-25 08:39:21 -0700

    list books and search books functions now work with SQL database

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-import { addBook, listBook } from "../controllers/controller.js";
+import { addBook, listBook, search } from "../controllers/controller.js";
 
 const router = express.Router();
 
 router.get("/bookList", listBook);
 
-//router.get("/search/:title", search);
+router.get("/search/:title", search);
 
 router.post("/addBook", addBook);
 
